7ba4d47c10f9d69d40b4a126944dbda936427c69
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2023-09-09 22:30:57 +0530

    Update PredefinedMethods.py

--- Python Project1/College Programs 1/String/PredefinedMethods.py
+++ Python Project1/College Programs 1/String/PredefinedMethods.py
@@ -9,25 +9,44 @@ print(s.startswith('A'))    # True
 print(s.endswith('n'))      # True
 
 print("\n")
-s = "My name is Aman005"
-print(s.isalpha())      # false
-print(s.isdigit())      # False
-print(s.isalnum())      # False
-print(s.islower())      # False
-print(s.isupper())      # False
-print(s.startswith('A'))    # False
-print(s.endswith('n'))      # False
+t = "My name is Aman005"
+print(t.isalpha())      # False
+print(t.isdigit())      # False
+print(t.isalnum())      # False
+print(t.islower())      # False
+print(t.isupper())      # False
+print(t.startswith('A'))    # False
+print(t.endswith('n'))      # False
 
 # 2 -> Search and Replace
 m = "Akhil"
 print(m.find('l'))      # 4
 print(m.replace('h',' '))   # Ak il
 n = "      Hello       "
-print(m.lstrip())
-print(m.rstrip())
-print(m.strip())
+print(n.lstrip())       # Hello
+print(n.rstrip())       #       Hello
+print(n.strip())        # Hello
 
 
-# 3 -> Split and Partition
+# 3 -> Split [returns list] and Partition [returns tuple]
 o = "Python Strings"
 print(o.split())    # ['Python', 'Strings']
+p = "My name is - Ajay"
+print(p.split('-'))     # ['My name is ', ' Ajay']
+q = "One-Two-Three-Four"
+print(q.split('-'))                     # ['One', 'Two', 'Three', 'Four']
+print(q.split('-' , maxsplit = 1))      # ['One', 'Two-Three-Four']  
+print(q.split('-' , maxsplit = 2))      # ['One', 'Two', 'Three-Four']
+print(q.split('-' , maxsplit = 5))      # ['One', 'Two', 'Three', 'Four']
+r = "I could eat apples everday"
+print(r.partition("apples"))            # ('I could eat ', 'apples', ' everday')
+
+# 4 -> Other Methods of string
+print(s.upper())        # AMAN
+print(s.lower())        # aman
+result = '-'.join(s)
+print(result)           # A-m-a-n
+u = "myself Ajay vishwakarma"
+print(u.capitalize())   # Myself ajay vishwakarma
+print(u.title())        # Myself Ajay Vishwakarma
+print(u.swapcase())     # MYSELF aJAY VISHWAKARMA
