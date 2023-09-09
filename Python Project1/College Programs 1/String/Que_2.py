9e3b5a226041336aba0c0c94f7aadefbf9a8861c
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2023-09-09 23:08:23 +0530

    Update Que_2.py

--- Python Project1/College Programs 1/String/Que_2.py
+++ Python Project1/College Programs 1/String/Que_2.py
@@ -1,10 +1,31 @@
-''' Input a string  and print length of the string '''
-s1 = input("Enter a string : ")
-print("String s1 length = ", len(s1))
+s = "Bamboozled"
+print(s[0:2])   # Ba
+print(s[8:10])  # ed
+print(s[2:])    # mboozled
+print(s[0:6])   # Bamboo
+print(s)        # Bamboozled
+print(s[0]+s[2]+s[4]+s[6]+s[8])     # Bmoze
+print(s[0]+s[3]+s[6]+s[9])          # Bbzd
+print(s[0]+s[5]+s[8])               # Boe
+print(s[0:6]+"Manager")             # BambooManger
 
-# Check if the string contains only alphanumeric characters
-if s1.isalpha():
-    print("String s1 =", s1)
-else:
-    print("Enter a string without using any digit value.")
-# && s1 != s1.isdigit()
+t = "Bring It On"
+print(t.upper())        # BRING IT ON
+print(t.lower())        # bring it on
+print(t.capitalize())   # Bring it on
+print(t.title())        # Bring It On
+print(t.swapcase())     # bRING iT oN
+print(t.replace("It","Him"))  # Bring Him On
+
+msg = "Keep Yourself Warm"
+print(msg.partition(' '))   # ('Keep', ' ', 'Yourself Warm')
+print(msg.split(' '))       # ['Keep', 'Yourself', 'Warm']
+print(msg.swapcase())       # kEEP yOURSELF wARM
+print(msg.count('e'))       # 3
+print(len(msg))             # 18
+print(msg[-1])              # m
+print(msg[:1])              # k
+print(msg[-1:3])            #
+print(msg[:-3])             # Keep Yourself W
+print(msg[-3:])             # arm
+print(msg[0:2])             # ke
